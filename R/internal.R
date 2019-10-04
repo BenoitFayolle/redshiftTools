58a39b51175a3e6ce018229eb4cc12b96b1cb79e
FAYOLLE <benoit.fayolle53@gmail.com>
2019-10-04 17:09:39 +0100

    create rs_move_table function new internal functions unloadToS3 update some internal functions

--- R/internal.R
+++ R/internal.R
@@ -26,7 +26,7 @@ uploadToS3 = function(data, bucket, split_files, key, secret, session, region){
     write.csv(part, gzfile(tmpFile, encoding="UTF-8"), na='', row.names=F, quote=T)
 
     r=put_object(file = tmpFile, object = s3Name, bucket = "", key=key, secret=secret,
-        session=session, region=region)
+                 session=session, region=region)
     pb$tick()
     return(r)
   }
@@ -42,20 +42,54 @@ uploadToS3 = function(data, bucket, split_files, key, secret, session, region){
   }
 }
 
-#' @importFrom "aws.s3" "delete_object"
+#' @importFrom "aws.s3" "bucket_exists"
+#' @importFrom "glue" "glue"
+unloadToS3 <- function(table_name,schema , bucket, dbcon, key, secret, session, region){
+
+  prefix=paste0(sample(rep(letters, 10),50),collapse = "")
+  if(!bucket_exists(bucket, key=key, secret=secret, session=session, region=region)){
+    stop("Bucket does not exist")
+  }
+
+
+  # pb <- progress_bar$new(total = split_files, format='Unloading file :current/:total [:bar]')
+  # pb$tick(0)
+
+  s3Name=paste0("s3://",bucket, "/", prefix)
+  statement <- glue("unload ('select * from {schema}.{table_name}')
+                  to '{s3Name}'
+                  ACCESS_KEY_ID '{key}'
+                  SECRET_ACCESS_KEY '{secret}'
+                  FORMAT AS CSV
+                  GZIP
+                  PARALLEL OFF
+                  MAXFILESIZE 100 MB
+                  ")
+  res <- dbExecute(dbcon,statement)
+
+  if(res!=0){
+    warning("Error uploading data!")
+    return(NA)
+  }else{
+    message("Upload to S3 complete!")
+    return(prefix)
+  }
+}
+
+
+#' @importFrom "aws.s3" "delete_object" "get_bucket_df"
 #' @importFrom "purrr" "map"
 deletePrefix = function(prefix, bucket, split_files, key, secret, session, region){
 
-  s3Names=paste(prefix, ".", formatC(1:split_files, width = 4, format = "d", flag = "0"), sep="")
-
+  s3Names = get_bucket_df(bucket,prefix) %>% filter(grepl(prefix,Key)) %>% {.$Key}
   message(paste("Deleting", split_files, "files with prefix", prefix, "from bucket", bucket))
 
-  pb <- progress_bar$new(total = split_files, format='Deleting file :current/:total [:bar]')
-  pb$tick(0)
+  # pb <- progress_bar$new(total = split_files, format='Deleting file :current/:total [:bar]')
+  # pb$tick(0)
 
   deleteObj = function(obj){
     delete_object(obj, bucket, key=key, secret=secret, session=session, region=region)
-    pb$tick()
+    # pb$tick()
   }
 
   res = map(s3Names, deleteObj)
@@ -97,24 +131,28 @@ splitDetermine = function(dbcon, numRows, rowSize){
 }
 
 
-s3ToRedshift = function(dbcon, table_name, bucket, prefix, region, access_key, secret_key, session, iam_role_arn, additional_params){
-    stageTable=paste0(sample(letters,16),collapse = "")
-    # Create temporary table for staging data
-    queryStmt(dbcon, sprintf("create temp table %s (like %s)", stageTable, table_name))
+s3ToRedshift = function(dbcon, table_name, bucket, prefix, region, access_key, secret_key, session, iam_role_arn, additional_params,is_gz=F){
+  stageTable=paste0(sample(letters,16),collapse = "")
+  # Create temporary table for staging data
+  queryStmt(dbcon, sprintf("create temp table %s (like %s)", stageTable, table_name))
+  if (is_gz)
+    copyStr = "copy %s from 's3://%s/%s' region '%s' csv gzip ignoreheader 1 emptyasnull COMPUPDATE FALSE STATUPDATE FALSE %s %s"
+  else
     copyStr = "copy %s from 's3://%s/%s.' region '%s' csv gzip ignoreheader 1 emptyasnull COMPUPDATE FALSE STATUPDATE FALSE %s %s"
-    # Use IAM Role if available
-    if (nchar(iam_role_arn) > 0) {
-      credsStr = sprintf("iam_role '%s'", iam_role_arn)
-    } else {
-      # creds string now includes a token in case it is needed.
-        if (session != '') {
-          credsStr = sprintf("credentials 'aws_access_key_id=%s;aws_secret_access_key=%s;token=%s'", access_key, secret_key, session)
-        } else {
-          credsStr = sprintf("credentials 'aws_access_key_id=%s;aws_secret_access_key=%s'", access_key, secret_key)
-        }
-    }
-    statement = sprintf(copyStr, stageTable, bucket, prefix, region, additional_params, credsStr)
-    queryStmt(dbcon, statement)
 
-    return(stageTable)
+  # Use IAM Role if available
+  if (nchar(iam_role_arn) > 0) {
+    credsStr = sprintf("iam_role '%s'", iam_role_arn)
+  } else {
+    # creds string now includes a token in case it is needed.
+    if (session != '') {
+      credsStr = sprintf("credentials 'aws_access_key_id=%s;aws_secret_access_key=%s;token=%s'", access_key, secret_key, session)
+    } else {
+      credsStr = sprintf("credentials 'aws_access_key_id=%s;aws_secret_access_key=%s'", access_key, secret_key)
+    }
+  }
+  statement = sprintf(copyStr, stageTable, bucket, prefix, region, additional_params, credsStr)
+  queryStmt(dbcon, statement)
+
+  return(stageTable)
 }
---
+++ R/move.R
@@ -0,0 +1,131 @@
+#' Move redshift table from one cluster to another
+#'
+#' Unload a table to S3 and then load it with redshift, replacing the contents of that table.
+#' The table on redshift has to have the same structure and column ordering to work correctly.
+#'
+#' @param table_name the name of the table to move
+#' @param schema_from the name of the schema where the source table is
+#' @param schema_to the name of the destination schema
+#' @param dbcon_from a db connection to the source RS cluster
+#' @param dbcon_to a db connection to the destination RS cluster
+#' @param split_files optional parameter to specify amount of files to split into. If not specified will look at amount of slices in Redshift to determine an optimal amount.
+#' @param bucket the name of the temporary bucket to load the data. Will look for AWS_BUCKET_NAME on environment if not specified.
+#' @param region the region of the bucket. Will look for AWS_DEFAULT_REGION on environment if not specified.
+#' @param access_key the access key with permissions for the bucket. Will look for AWS_ACCESS_KEY_ID on environment if not specified.
+#' @param secret_key the secret key with permissions fot the bucket. Will look for AWS_SECRET_ACCESS_KEY on environment if not specified.
+#' @param session_token the session key with permissions for the bucket, this will be used instead of the access/secret keys if specified. Will look for AWS_SESSION_TOKEN on environment if not specified.
+#' @param iam_role_arn an iam role arn with permissions fot the bucket. Will look for AWS_IAM_ROLE_ARN on environment if not specified. This is ignoring access_key and secret_key if set.
+#' @param wlm_slots amount of WLM slots to use for this bulk load http://docs.aws.amazon.com/redshift/latest/dg/tutorial-configuring-workload-management.html
+#' @param additional_params Additional params to send to the COPY statement in Redshift
+#'
+#' @examples
+#' library(DBI)
+#'
+#' a=data.frame(a=seq(1,10000), b=seq(10000,1))
+#'
+#'\dontrun{
+#' dbcon_from <- dbConnect(RPostgres::Postgres(), dbname="dbname",
+#' host='my-redshift-url.amazon.com', port='5439',
+#' user='myuser', password='mypassword',sslmode='require')
+#'  dbcon_to <- dbConnect(RPostgres::Postgres(), dbname="dbname2",
+#' host='my-redshift-url2.amazon.com', port='5439',
+#' user='myuser', password='mypassword2',sslmode='require')
+
+#'
+#' rs_move_table(table_name,"public","public",dbcon_from,dbcom_to,
+#' bucket="my-bucket", split_files=4)
+#'
+#' }
+#' @export
+rs_move_table = function(
+  table_name,
+  schema_from,
+  schema_to,
+  dbcon_from,
+  dbcon_to,
+  split_files,
+  bucket=Sys.getenv('AWS_BUCKET_NAME'),
+  region=Sys.getenv('AWS_DEFAULT_REGION'),
+  access_key=Sys.getenv('AWS_ACCESS_KEY_ID'),
+  secret_key=Sys.getenv('AWS_SECRET_ACCESS_KEY'),
+  session_token=Sys.getenv('AWS_SESSION_TOKEN'),
+  iam_role_arn=Sys.getenv('AWS_IAM_ROLE_ARN'),
+  wlm_slots=1,
+  additional_params=''){
+
+  message('Checking table exists in destination')
+
+  svv_to <- dbGetQuery(dbcon_to,glue("select * from svv_table_info where schema = '{schema_to}' and \"table\" = '{table_name}'"))
+
+  if(nrow(svv_to) != 1){
+    message("Table doesn't exist in destination. Creating..")
+    ddl <- dbGetQuery(dbcon_from,glue("select ddl from admin.v_generate_tbl_ddl where tablename = '{table_name}'  and schemaname = '{schema_from}'"))
+    if(nrow(ddl) == 0){
+      warning("ddl not found. Please generate it with the amazon-redshift-utils command.
+              See https://github.com/awslabs/amazon-redshift-utils/blob/master/src/AdminViews/v_generate_tbl_ddl.sql")
+      return(FALSE)
+    }
+    ddl_statement <- ddl %>% {paste(.$ddl,collapse="")}
+    message(paste("creating table with following ddl\n",ddl_statement))
+    ddl_res <- strsplit(x=ddl_statement,split=";")[[1]] %>% map(function(x){dbExecute(dbcon_to,x)})
+  }
+
+  message('Initiating Redshift table moving for table ',table_name)
+
+  svv <- dbGetQuery(dbcon_from,glue("select * from svv_table_info where schema = '{schema_from}' and \"table\" = '{table_name}'"))
+
+  if(nrow(svv) != 1){
+    warning("Cannot locate the source table")
+    return(FALSE)
+  }
+
+  numRows <- svv$tbl_rows
+
+  if(numRows == 0){
+    warning("Empty dataset provided, will not try uploading")
+    return(FALSE)
+  }
+
+  message(paste0("The provided data.frame has ", numRows, ' rows '))
+
+
+  # Unload tabke to S3
+  prefix = unloadToS3(table_name,schema_from, bucket, dbcon_from, access_key, secret_key, session_token, region)
+
+  if(wlm_slots>1){
+    queryStmt(dbcon,paste0("set wlm_query_slot_count to ", wlm_slots));
+  }
+  schema_table = paste0(schema_to,".",table_name)
+  result = tryCatch({
+    stageTable=s3ToRedshift(dbcon_to, schema_table, bucket, prefix, region,
+                            access_key, secret_key, session_token, iam_role_arn, additional_params,
+                            is_gz=T)
+
+    # Use a single transaction
+    queryStmt(dbcon_to, 'begin')
+
+    message("Deleting target table for replacement")
+    queryStmt(dbcon_to, sprintf("delete from %s", schema_table))
+
+    message("Insert new rows")
+    queryStmt(dbcon_to, sprintf('insert into %s select * from %s', schema_table, stageTable))
+
+    message("Drop staging table")
+    queryStmt(dbcon_to, sprintf("drop table %s", stageTable))
+
+    message("Committing changes")
+    queryStmt(dbcon_to, "COMMIT;")
+
+    return(TRUE)
+  }, error = function(e) {
+    warning(e$message)
+    queryStmt(dbcon_to, 'ROLLBACK;')
+    return(FALSE)
+  }, finally = {
+
+    message("Deleting temporary files from S3 bucket")
+    deletePrefix(prefix, bucket, split_files, access_key, secret_key, session_token, region)
+  })
+
+  return (result)
+}
